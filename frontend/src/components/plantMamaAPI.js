7a57bdbd265d387b1425919847ef0f8c9811b5a6
Anastasiia Derkach <anastasiiaderkach@Anastasiias-MacBook-Air.local>
2025-04-21 19:59:03 +0100

    Preparing for frontend deployment with replacing the backend URL to new host

--- frontend/src/components/plantMamaAPI.js
+++ frontend/src/components/plantMamaAPI.js
@@ -2,12 +2,12 @@
 // src/components/plantMamaAPI.js
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000'; // Update this to your deployed backend URL if needed
+const BASE_URL = 'https://plantmama-backend.onrender.com'; 
 
 export const sendTextQuery = async (message) => {
   try {
     const res = await axios.post(`${BASE_URL}/chat`, { message });
-    return res.data; // should contain { reply: "..." }
+    return res.data; // 
   } catch (error) {
     throw new Error('Text query failed.');
   }
@@ -21,7 +21,7 @@ export const sendImageQuery = async (imageFile) => {
     const res = await axios.post(`${BASE_URL}/predict-chatbot`, formData, {
       headers: { 'Content-Type': 'multipart/form-data' }
     });
-    return res.data; // should contain { plant, disease, chatbot_message }
+    return res.data; 
   } catch (error) {
     throw new Error('Image query failed.');
   }
