5ef62cdf59ad1879f5d4dfb83d0e6b31f7080ca1
Anastasiia Derkach <anastasiiaderkach@Anastasiias-MacBook-Air.local>
2025-04-21 22:46:34 +0100

    Debugging api interaction

--- frontend/src/components/plantMamaAPI.js
+++ frontend/src/components/plantMamaAPI.js
@@ -5,13 +5,15 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const sendTextQuery = async (message) => {
-  try {
-    const res = await axios.post(`${BASE_URL}/chat`, { message });
-    return res.data; // 
-  } catch (error) {
-    throw new Error('Text query failed.');
-  }
-};
+    try {
+      const res = await axios.post(`${BASE_URL}/chat`, { message });
+      console.log('Response from backend:', res); 
+      return res.data; 
+    } catch (error) {
+      console.error('❌ sendTextQuery failed:', error); 
+      throw new Error('Text query failed.');
+    }
+  };
 
 export const sendImageQuery = async (imageFile) => {
   const formData = new FormData();
